be1cbf5b3a006c79bdabc730b4d20597cfa24aa4
Clebson Moura <clebson.sh@gmail.com>
2020-09-03 10:40:11 -0300

    basic CRUD for users route done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Node rodando com sucesso!')
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const userRoute = require('./routes/userRoute')
+
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const port = 3000
+
+userRoute(app)
+
+app.get('/', (req, res) => res.send('Olá mundo pelo express!'))
+
+app.listen(port, () => console.log(`Api rodando na port ${port}`))
